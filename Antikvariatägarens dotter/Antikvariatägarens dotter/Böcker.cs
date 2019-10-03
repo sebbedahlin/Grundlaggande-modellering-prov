19c6d3c0d751175e6f4bb71dbaef2cd4b079a586
sebbedahlin <sebbedahlin@gmail.com>
2019-10-03 13:43:10 +0200

    Böcker

---
+++ Antikvariatägarens dotter/Antikvariatägarens dotter/Böcker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatägarens_dotter
+{
+    class Böcker
+    {
+        private string name;
+        private string category;
+        public int price = 0;
+        private int rarity = 0;
+        private int actualValue = 0;
+        private bool cursed = true;
+        private static Random gen = new Random();
+        
+    }
+
+    public Book()
+    {
+        Random actualValue
+    }   
+
+    public void Printinfo()
+    {
+        Console.WriteLine("Book name:");
+        Console.WriteLine("Rarity:");
+        Console.WriteLine("Category:");
+        Console.WriteLine("Price:");
+    }
+
+    public int Evaluate()
+    {
+       
+    }
+
+    public string GetCategory()
+    {
+        return categoryValue;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public bool IsCursed()
+    {
+        return true == 
+    }
+}
---
+++ Antikvariatägarens dotter/Antikvariatägarens dotter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatägarens_dotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Böcker böcker = new Böcker();
+        }
+    }
+}
